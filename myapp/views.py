bbb589c6e8161d053e92de7931db8f0dfd190d57
Marcos Alvez <alvezmarcos2003@gmail.com>
2024-10-17 18:54:19 -0300

    Reorganizar y corregir models, urls, y views

--- myapp/views.py
+++ myapp/views.py
@@ -5,4 +5,6 @@ from .models import ExampleModel1
 def example_view(request):
     examples = ExampleModel1.objects.all()
     return render(request, 'example_template.html', {'examples': examples})
-    
+
+def mi_vista(request):
+    return render(request, 'otra_plantilla.html')
